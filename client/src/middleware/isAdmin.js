761798d1a848ffe09c5f4fa4ec237d1212893a4f
alefer64 <144471872+alefer64@users.noreply.github.com>
2024-11-25 13:44:49 -0300

    Delete client directory

--- client/src/middleware/isAdmin.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const isAdmin = (user) => {
-  if (!user || !user.isAdmin) {
-    return <Navigate to="/dashboard" />;
-  }
-};
-
-export default isAdmin;
